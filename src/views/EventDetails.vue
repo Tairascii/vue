287ed406c3db44264733c07646736a4ff98cb1b5
Tairascii <64748528+Tairascii@users.noreply.github.com>
2021-12-30 20:37:58 +0600

    Add files via upload

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventServices from '../Services/EventServices'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+    }
+  },
+  created() {
+    EventServices.getEvent(this.id)
+      .then((res) => {
+        this.event = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+}
+</script>
---
+++ src/views/EventList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="events">
+    <h1>Event For Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventServices from '../Services/EventServices'
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    }
+  },
+  created() {
+    EventServices.getEvents()
+      .then((res) => {
+        this.events = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
